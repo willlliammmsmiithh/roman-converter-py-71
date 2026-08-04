58dbacbaa55dd980d72daa1606a75593f91de52b
willlliammmsmiithh <81@felix-au.me>
2026-08-04 19:02:00 +0530

    Update roman.py - Step 7

--- roman.py
+++ roman.py
@@ -1,3 +1,6 @@
 def roman_to_int(s):
     mapping = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100}
     total = 0
+    for i in range(len(s)):
+        val = mapping.get(s[i], 0)
+        if i + 1 < len(s) and val < mapping.get(s[i+1], 0):
